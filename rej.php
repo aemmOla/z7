41c4c671c911a549012995bcb9dc3ad6e505cce6
aemmOla <35296070+aemmOla@users.noreply.github.com>
2018-01-10 14:12:07 +0100

    Update rej.php

--- rej.php
+++ rej.php
@@ -4,7 +4,7 @@
 </head>
 <body>
 <?php
-$dbhost="localhost"; $dbuser="aemmpl_ola"; $dbpassword="baza"; $dbname="aemmpl_bazola"; 
+$dbhost="localhost"; $dbuser="aemmpl_ola"; $dbpassword="XXXXX"; $dbname="aemmpl_bazola"; 
 $polaczenie = mysqli_connect ($dbhost, $dbuser, $dbpassword);                       //łączenie z bazą danych
 mysqli_select_db ($polaczenie, $dbname);
 $imie = $_POST['imie'];
